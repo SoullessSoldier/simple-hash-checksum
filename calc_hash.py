b1cb3412dd366d7db72b70fb0174556ce7c0c813
SoullessSoldier <48070769+SoullessSoldier@users.noreply.github.com>
2020-01-09 23:11:06 +0200

    Add second parameter support

--- calc_hash.py
+++ calc_hash.py
@@ -8,41 +8,45 @@ md5 = hashlib.md5()
 sha1 = hashlib.sha1()
 sha256=hashlib.sha256()
 
-with open(sys.argv[1], 'rb') as f:
-    while True:
-        data = f.read(BUF_SIZE)
-        if not data:
-            break
-        md5.update(data)
-        sha1.update(data)
-        sha256.update(data)
+if len(sys.argv)==1:
+    print("Usage: python.exe calc_hash.py [file_to_be_hashed]")
+    print("Or usage python.exe calc_hash.py [file_to_be_hashed] [file_with_checksum_to_compare_with]")
 
-print("MD5: {0}".format(md5.hexdigest()))
-print("SHA1: {0}".format(sha1.hexdigest()))
-print("SHA256: {0}".format(sha256.hexdigest()))
-'''
-with open(sys.argv[2], 'rb') as f:
+if len(sys.argv)==2:
+    with open(sys.argv[1], 'rb') as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            md5.update(data)
+            sha1.update(data)
+            sha256.update(data)
+    with open(sys.argv[1]+"_hash.txt","w") as f:
+        print("MD5: {0}".format(md5.hexdigest()),file=f)
+        print("SHA1: {0}".format(sha1.hexdigest()),file=f)
+        print("SHA256: {0}".format(sha256.hexdigest()),file=f)
+elif len(sys.argv)==3:
+    with open(sys.argv[1], 'rb') as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            md5.update(data)
+            sha1.update(data)
+            sha256.update(data)
+
+    print("MD5: {0}".format(md5.hexdigest()))
+    print("SHA1: {0}".format(sha1.hexdigest()))
+    print("SHA256: {0}".format(sha256.hexdigest()))
+    with open(sys.argv[2], 'r') as f:
         lines=f.readlines()
         for line in lines:
             if line.startswith("SHA256"):
-                sha=line.split(": ").strip()
+                sha=line.split(": ")[1].strip()
+                if sha.isupper():
+                    sha=sha.lower()
+                print(f"sha in hash-file: {sha}")
                 if sha==sha256.hexdigest():
                     print("SHA256 hashes are equal")
                 else:
                     print("SHA256 hashes are NOT equal")
-'''                    
-
-'''
-def get_digest(file_path):
-    h = hashlib.sha256()
-
-    with open(file_path, 'rb') as file:
-        while True:
-            # Reading is buffered, so we can read smaller chunks.
-            chunk = file.read(h.block_size)
-            if not chunk:
-                break
-            h.update(chunk)
-
-    return h.hexdigest()
-'''
