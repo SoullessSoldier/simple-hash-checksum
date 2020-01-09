37ec2a1ade2b114a61b6c3029c308fdc6b608b2e
SoullessSoldier <48070769+SoullessSoldier@users.noreply.github.com>
2020-01-09 13:21:59 +0200

    Hash calculator
    Hash calculator  
    https://stackoverflow.com/questions/22058048/hashing-a-file-in-python

---
+++ calc_hash.py
@@ -0,0 +1,38 @@
+import sys
+import hashlib
+
+# BUF_SIZE is totally arbitrary, change for your app!
+BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
+
+md5 = hashlib.md5()
+sha1 = hashlib.sha1()
+sha256=hashlib.sha256()
+
+with open(sys.argv[1], 'rb') as f:
+    while True:
+        data = f.read(BUF_SIZE)
+        if not data:
+            break
+        md5.update(data)
+        sha1.update(data)
+        sha256.update(data)
+
+print("MD5: {0}".format(md5.hexdigest()))
+print("SHA1: {0}".format(sha1.hexdigest()))
+print("SHA256: {0}".format(sha256.hexdigest()))
+
+
+'''
+def get_digest(file_path):
+    h = hashlib.sha256()
+
+    with open(file_path, 'rb') as file:
+        while True:
+            # Reading is buffered, so we can read smaller chunks.
+            chunk = file.read(h.block_size)
+            if not chunk:
+                break
+            h.update(chunk)
+
+    return h.hexdigest()
+'''
